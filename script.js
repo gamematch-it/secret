0fa26f1fc421dc9100cbd2733b03d0d262d7c95f
gamematch-it <gamematchit@gmail.com>
2025-02-12 20:19:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function reply() {
+    alert("I LOVE YOU SO MUCH! You'll be my Valentine forever!");
+}
+
+document.getElementById("BawalButton").addEventListener("click", function() {
+    var reconsider = confirm("Kawawa naman ako, reconsideration please 🥺");
+    if (reconsider) {
+        alert("WOWOWOWOW! Thank you, baby!");
+    } else {
+        alert("Say yes na, mahal 🥺 !!!");
+    }
+});
